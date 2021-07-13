a58c13a8a38b4bdcd2dfbfd00ff1eb69742a1c8c
dooyeong20 <noviced22@gmail.com>
2021-07-13 14:35:45 +0900

    feat: Stopwatch start and display function implemented
    Stopwatch display & formating method

--- stopwatch.js
+++ stopwatch.js
@@ -6,3 +6,42 @@ let time = {
   sec: 0,
   ms: 0,
 };
+
+const util = (() => {
+  const formatTime = (time) => (time < 10 ? '0' + time : time);
+  return { formatTime };
+})();
+
+const stopWatch = (() => {
+  const $min = document.querySelector('.display-m');
+  const $sec = document.querySelector('.display-s');
+  const $ms = document.querySelector('.display-ms');
+  const $startBtn = document.querySelector('.startBtn');
+  const $lapBtn = document.querySelector('.lapBtn');
+  const $resetBtn = document.querySelector('.resetBtn');
+
+  const setTime = (_ms) => {
+    if (time.ms >= 100) {
+      time.ms = 0;
+      time.sec++;
+      if (time.sec >= 60) {
+        time.sec = 0;
+        time.min++;
+      }
+    }
+
+    $min.textContent = util.formatTime(time.min);
+    $sec.textContent = util.formatTime(time.sec);
+    $ms.textContent = util.formatTime(time.ms);
+  };
+
+  const start = () => {
+    timerId = setInterval(() => {
+      setTime(time.ms++);
+    }, 10);
+  };
+
+  return { start };
+})();
+
+stopWatch.start();
